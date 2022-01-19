66e31c767d9ae585d2a0439a233c38c33132851a
ErinScribner <scribner.e@digipen.edu>
2022-01-19 15:44:11 -0800

    ErinScribner-Made it so that when i is pressed, an ice block appears below the player

--- prototyping1/Assets/Scripts/StudentScripts/ErinScribner/ErinScribner_IcePowers.cs
+++ prototyping1/Assets/Scripts/StudentScripts/ErinScribner/ErinScribner_IcePowers.cs
@@ -6,15 +6,26 @@ using UnityEngine;
 //player character is looking at
 public class ErinScribner_IcePowers : MonoBehaviour
 {
+    private GameHandler gameHandlerObj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (GameObject.FindGameObjectWithTag("GameHandler") != null)
+        {
+            gameHandlerObj = GameObject.FindGameObjectWithTag("GameHandler").GetComponent<GameHandler>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            GameObject iceblock = GameObject.Find("IceBlock");
+            GameObject player = GameObject.Find("Player");
+
+            iceblock.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - .8f, player.transform.position.z);
+        }
     }
 }
