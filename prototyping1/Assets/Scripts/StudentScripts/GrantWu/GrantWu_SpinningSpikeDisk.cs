57b78d64635427dc52764160be61dc6ade0dbd6b
grant.wu <gwu3405@gmail.com>
2022-01-20 13:55:38 -0800

    Grant Wu: Adjusted public values

--- prototyping1/Assets/Scripts/StudentScripts/GrantWu/GrantWu_SpinningSpikeDisk.cs
+++ prototyping1/Assets/Scripts/StudentScripts/GrantWu/GrantWu_SpinningSpikeDisk.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class GrantWu_SpinningSpikeDisk : MonoBehaviour
 {
-    public Vector3 pointA;
-    public Vector3 pointB;
+    public float y_dist;
+    public float x_dist;
+    public float y_center;
+    public float x_center;
+
     public float speed;
     public bool reverse_speed;
+    
 
-
-    private float y_center;
-    private float y_dist;
     // Start is called before the first frame update
     void Start()
     {
-        y_dist = Mathf.Abs(pointA.y - pointB.y);
-        y_center = Mathf.Ceil((pointA.y + pointB.y) / 2);
     }
 
     // Update is called once per frame
