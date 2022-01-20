d66914028f9050f7918b46355bc0eea6fed4d610
gwu3405 <gwu3405@gmail.com>
2022-01-19 19:54:41 -0800

    Grant Wu: Spinning Disk working
    Requires a center point, Two locations to move to and from, speed of blade, and can reverse direction

--- prototyping1/Assets/Scripts/StudentScripts/GrantWu/GrantWu_SpinningSpikeDisk.cs
+++ prototyping1/Assets/Scripts/StudentScripts/GrantWu/GrantWu_SpinningSpikeDisk.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GrantWu_SpinningSpikeDisk : MonoBehaviour
 {
+    public float y_center;
+    public Vector3 pointA;
+    public Vector3 pointB;
+    public float speed;
+    public bool reverse_speed;
+
+
+    private float y_dist;
     // Start is called before the first frame update
     void Start()
     {
-        
+        y_dist = Mathf.Abs(pointA.y - pointB.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (reverse_speed)
+            transform.position = new Vector3(transform.position.x, y_center + -(Mathf.PingPong(Time.time * speed, y_dist) - y_dist / 2f), transform.position.z);
+        else
+            transform.position = new Vector3(transform.position.x, y_center + Mathf.PingPong(Time.time * speed, y_dist) - y_dist / 2f, transform.position.z);
     }
 }
