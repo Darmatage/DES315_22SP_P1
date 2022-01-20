1c3702c0cc5a5a402f3510c84c660e1c73e79339
Chris Dowell <chrisdowell6@gmail.com>
2022-01-19 22:55:34 -0800

    Barrel now times up and grows in size. Does not explode
    Wont explode

--- prototyping1/Assets/Scripts/StudentScripts/ChrisDowell/ExplosiveBarrel.cs
+++ prototyping1/Assets/Scripts/StudentScripts/ChrisDowell/ExplosiveBarrel.cs
@@ -17,7 +17,7 @@ public class ExplosiveBarrel : MonoBehaviour
         [Header("Statistics")]
         public int m_maxHP = 10;
         public int m_damage = 25;
-        public float m_explosiveRadius = 1f;
+        public float m_explosiveRadius = 2f;
         public float m_pushforce = 1;
         [System.NonSerialized] public bool m_exploded;
     }
@@ -26,13 +26,15 @@ public class ExplosiveBarrel : MonoBehaviour
     public class BarrelVisualData
     {
         public Gradient m_barrelColors;
-        public Vector2 m_newscale;
+        public Vector2 m_newScale;
     }
 
     public BarrelGameData m_stats;
     public BarrelVisualData m_visuals;
+    public LayerMask m_damageMask;
+    //public LayerMask m_deleteMask; // Deletes objects if explodes
 
-
+    private GameHandler m_handler;
     private Vector3 m_initscale = Vector3.one;
     private Vector3 m_newScale;
     private SpriteRenderer m_renderer;
@@ -41,13 +43,15 @@ public class ExplosiveBarrel : MonoBehaviour
     private void Awake()
     {
         m_initscale = transform.localScale;
-        m_newScale = m_visuals.m_newscale;
+        m_newScale = transform.localScale * m_visuals.m_newScale;
     }
 
     void Start()
     {
         m_renderer = GetComponent<SpriteRenderer>();
         m_rb = GetComponent<Rigidbody2D>();
+        m_handler = FindObjectOfType<GameHandler>();
+        
     }
 
     // Update is called once per frame
@@ -93,7 +97,7 @@ public class ExplosiveBarrel : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-         
+        TakeDamage(1);
     }
 
     private void Explode()
@@ -102,12 +106,46 @@ public class ExplosiveBarrel : MonoBehaviour
 
         // Take everythign in the radius and deal damage and push them back
 
-        var hit = Physics2D.CircleCastAll(transform.position, m_stats.m_explosiveRadius, Vector2.zero);
+        //LayerMask affectMask = m_damageMask & m_deleteMask;
+
+        //var explosiveradius = GetComponent<CircleCollider2D>();
+        //List<Collider2D> hitlist = new List<Collider2D>();
+        //ContactFilter2D filter = new ContactFilter2D();
+        //filter.SetLayerMask(m_damageMask);
+        //int count = explosiveradius.OverlapCollider(filter, hitlist);
+
+        Vector2 dir = m_rb.velocity.normalized;
+
+        var hit = Physics2D.CircleCastAll(transform.position, m_stats.m_explosiveRadius, dir, .2f);
         
         foreach (var obj in hit)
         {
+            // Get vector from object ot target
+            obj.rigidbody.AddForce(obj.normal * m_stats.m_pushforce);
+
+            if (obj.collider.tag == "Player")
+            {
+                m_handler.TakeDamage(m_stats.m_damage);
+                continue;
+            }
+
+
+            // Get come component script and add it here to deal damage since we dont have some externalized health script?!?!?!!?
+            // TODO: figure out how to damage multiple enemies with DIFFERING SCRIPTS LIKE HOLY SHIT WHY
+            if (obj.collider.tag == "Enemy")
+            {
+                // I gotta somehow adjust their hp lol
+
+                // I will add some damage stuff later
+                // If you end up using my prefab please let me know on teams (c.dowell@digipen.edu) or discord (Frost#0006)
+                // DISCORD IS PREFERRED IM MOST LIKELY GOING TO IGNORE TEAMS
+
+
+            }
 
         }
+
+        GameObject.Destroy(this);
 
 
 
