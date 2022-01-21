4faa9e9709b7ce5449da95af5d9e40b24db6bc2b
EJH <89428796+ryaa1234@users.noreply.github.com>
2022-01-21 10:46:51 -0800

    Eunjin Hong - changed button

--- prototyping1/Assets/Scripts/StudentScripts/EunjinHong/EunjinHong_DashMove.cs
+++ prototyping1/Assets/Scripts/StudentScripts/EunjinHong/EunjinHong_DashMove.cs
@@ -1,82 +1,3 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class EunjinHong_DashMove : MonoBehaviour
-//{
-//    private Rigidbody2D rb;
-//    public float dashSpeed;
-//    private float dashTime;
-//    public float startDashTime;
-//    private int direction;
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        rb = GetComponent<Rigidbody2D>();
-//        dashTime = startDashTime;
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        if(direction == 0)
-//        {
-//            if(Input.GetKeyDown(KeyCode.LeftArrow))
-//            {
-//                direction = 1;
-//            }
-//            else if(Input.GetKeyDown(KeyCode.RightArrow))
-//            {
-//                direction = 2;
-//            }
-//            else if (Input.GetKeyDown(KeyCode.UpArrow))
-//            {
-//                direction = 3;
-//            }
-//            else if (Input.GetKeyDown(KeyCode.DownArrow))
-//            {
-//                direction = 4;
-//            }
-//        }
-//        else
-//        {
-//            if(dashTime <= 0)
-//            {
-//                direction = 0;
-//                dashTime = startDashTime;
-//                rb.velocity = Vector2.zero;
-//            }
-//            else
-//            {
-//                dashTime -= Time.deltaTime;
-
-//                if(direction == 1)
-//                {
-//                    rb.velocity = Vector2.left * dashSpeed;
-
-//                }
-//                if (direction == 2)
-//                {
-//                    rb.velocity = Vector2.right * dashSpeed;
-
-//                }
-//                if (direction == 3)
-//                {
-//                    rb.velocity = Vector2.up * dashSpeed;
-
-//                }
-//                if (direction == 4)
-//                {
-//                    rb.velocity = Vector2.down * dashSpeed;
-
-//                }
-//            }
-//        }
-//    }
-//}
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -104,7 +25,7 @@ public class EunjinHong_DashMove : MonoBehaviour
     {
         if (isDashing == false)
         {
-            isDashing = Input.GetKey(KeyCode.F);
+            isDashing = Input.GetKey(KeyCode.LeftShift);
             timer = 0.0f;
 
         }
