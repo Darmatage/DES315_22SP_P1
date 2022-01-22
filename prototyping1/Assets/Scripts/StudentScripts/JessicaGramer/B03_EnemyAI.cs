73dfe9dea53a09bf1a3dfc150e41189496f9cb37
Jessa Grammar <j.gramer@digipen.edu>
2022-01-21 17:31:51 -0800

    Jessica Gramer - Added A* Pathfinding algrothem

---
+++ prototyping1/Assets/Scripts/StudentScripts/JessicaGramer/B03_EnemyAI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(B03_AStarPathFinding))]
+public class B03_EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void fieldOfView()
+    {
+
+    }
+
+    bool findPlayer()
+    {
+        return false;
+    }
+
+    bool seekPlayer()
+    {
+        return false;
+    }
+}
