6f2c2cf3e919ecd8f17ebd9dcad60c213c22e917
WonJu Jo <wonju.cho@digipen.edu>
2022-01-24 13:05:00 -0800

    WonjuJo: Change Explosion Enemy prefab

--- prototyping1/Assets/Scripts/StudentScripts/WonjuJo/WonjuJo_Explode.cs
+++ prototyping1/Assets/Scripts/StudentScripts/WonjuJo/WonjuJo_Explode.cs
@@ -8,6 +8,7 @@ public class WonjuJo_Explode : MonoBehaviour
 
     private WonjuJo_PlayerHandler gameHandlerObj;
 
+    private WonjuJo_MonsterHandler MH;
     // Start is called before the first frame update
 
     private bool Once = false;
@@ -26,19 +27,25 @@ public class WonjuJo_Explode : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-      if (ExplosionEnemy.GetIsDead() && !Once)
+        if (ExplosionEnemy.GetIsDead() && !Once)
         {
-            StartCoroutine("Delay");
-            gameHandlerObj.TakeDamage(20);
-            Once = true;
+            if (collision.gameObject.tag == "Player")
+            {
+                Once = true;
 
+                StartCoroutine("Delay");
+            }
         }
+
+
     }
 
     IEnumerator Delay()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(2f);
+
+        gameHandlerObj.TakeDamage(20);
     }
 }
