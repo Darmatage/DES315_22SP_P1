9336d65483ba078828a3bfcc49091d34b10fa521
Frankie Camarillo <frankiecama@live.com>
2022-01-26 08:33:36 -0800

    Added changes to files, Reanmed files with my name in them

--- prototyping1/Assets/Scripts/StudentScripts/FrankieCamarillo/FrankieCamarilloDeleteOther.cs
+++ prototyping1/Assets/Scripts/StudentScripts/FrankieCamarillo/FrankieCamarilloDeleteOther.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeleteOther : MonoBehaviour
+public class FrankieCamarilloDeleteOther : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +14,16 @@ public class DeleteOther : MonoBehaviour
     void Update()
     {
         
+
+        
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        //test
         foreach (ContactPoint contact in collision.contacts)
         {
-            // object is in enemy layer
+            //object is in enemy layer
             if (contact.otherCollider.gameObject.layer == 6)
                 Destroy(contact.otherCollider.gameObject);
         }
