476702b908d12486a5f6e60f22f92ce00070762d
Charles O <mcfx2@mcfx2.co>
2022-01-26 21:55:12 -0800

    charles o: new portal feedback integrated

--- prototyping1/Assets/Scripts/StudentScripts/CharlesOsberg/PortalTeleport.cs
+++ prototyping1/Assets/Scripts/StudentScripts/CharlesOsberg/PortalTeleport.cs
@@ -14,6 +14,9 @@ public class PortalTeleport : MonoBehaviour
 
     private const float portalExclusionZone = 1.0f;
 
+
+    [SerializeField] private GameObject portalFizz = null;
+    
     private new Rigidbody2D rigidbody;
 
     public enum HitDirection
@@ -102,7 +105,7 @@ public class PortalTeleport : MonoBehaviour
             if (isValidTele)
             {
                 //push away a bit so we don't immediately teleport back and forth
-                portalDist += (other.transform.position - transform.position).normalized * pushAmt;
+                portalDist -= (other.transform.position - transform.position);
 
                 //also push away from contacted wall
                 portalDist += oDir switch
@@ -196,6 +199,7 @@ public class PortalTeleport : MonoBehaviour
 
             if (shouldFizzle)
             {
+                Instantiate(portalFizz, transform.position, transform.rotation);
                 Destroy(gameObject);
             }
         }
