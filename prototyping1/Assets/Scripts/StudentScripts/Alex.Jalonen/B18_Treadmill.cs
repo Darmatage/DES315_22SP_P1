236a8b37d524e39bc6532687b20460fcdda14d1b
Alex-Jalonen <71279927+Alex-Jalonen@users.noreply.github.com>
2022-01-27 23:18:50 -0800

    Alex Jalonen - Updated treadmills to work with proper physics

--- prototyping1/Assets/Scripts/StudentScripts/Alex.Jalonen/B18_Treadmill.cs
+++ prototyping1/Assets/Scripts/StudentScripts/Alex.Jalonen/B18_Treadmill.cs
@@ -2,16 +2,128 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class B18_Treadmill : MonoBehaviour
 {
+    public enum Speed
+    {
+        Slow,
+        Medium,
+        Fast
+    }
+
+    public bool setSpeed = false;
+    public float slowSpeed_;
+    public float medSpeed_;
+    public float fastSpeed_;
+
+    private static float slowSpeed = 20f;
+    private static float mediumSpeed = 30f;
+    private static float fastSpeed = 50f;
+
+    public Speed speed;
+
+    private float tileSpeed = 0f;
+
+    private static Dictionary<int, Vector2> moveList = new Dictionary<int, Vector2>();
+    private static Dictionary<int, bool> updateList = new Dictionary<int, bool>();
+
+    private void Start()
+    {
+        switch(speed)
+        {
+            case Speed.Slow: tileSpeed = slowSpeed; break;
+            case Speed.Medium: tileSpeed = mediumSpeed; break;
+            case Speed.Fast: tileSpeed = fastSpeed; break;
+        }
+
+        slowSpeed_ = slowSpeed; medSpeed_ = mediumSpeed; fastSpeed_ = fastSpeed;
+
+
+        gameObject.GetComponent<Animator>().speed =  (tileSpeed / mediumSpeed) * 4.2f;
+    }
+
+    private void Update()
+    {
+        if(setSpeed)
+        {
+            slowSpeed = slowSpeed_; mediumSpeed = medSpeed_; fastSpeed = fastSpeed_;
+        }
+        else
+        {
+            slowSpeed_ = slowSpeed; medSpeed_ = mediumSpeed; fastSpeed_ = fastSpeed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foreach(var key in new List<int>(updateList.Keys))
+        {
+            updateList[key] = false;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var otherRB = collision.gameObject.GetComponent<Rigidbody2D>();
+
+        if (otherRB != null)
+        {
+            var otherRBID = otherRB.GetInstanceID();
+
+            Vector2 direction;
+
+            if (moveList.ContainsKey(otherRBID))
+            {
+                var vec = moveList[otherRBID];
+                vec += (Vector2)gameObject.transform.up;
+                moveList[otherRBID] = vec;
+            }
+            else
+            {
+                var vec = (Vector2)gameObject.transform.up;
+                moveList.Add(otherRBID, vec);
+            }
+
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         var otherRB = collision.gameObject.GetComponent<Rigidbody2D>();
 
         if(otherRB != null)
         {
-            otherRB.velocity += (Vector2)gameObject.transform.up * 3f;
-        }
+            var otherRBID = otherRB.GetInstanceID();
+
+            if (updateList.ContainsKey(otherRBID) && updateList[otherRBID] == true) return;
+
+            Vector2 direction;
+
+            if(moveList.ContainsKey(otherRBID))
+            {
+                var vec = moveList[otherRBID];
+                direction = vec.normalized;
+            }
+            else
+            {
+                var vec = (Vector2)gameObject.transform.up;
+                moveList.Add(otherRBID, vec);
+                direction = vec.normalized;
+            }
+
+            otherRB.velocity += direction * tileSpeed * Time.deltaTime;
+
+            if(updateList.ContainsKey(otherRBID))
+            {
+                updateList[otherRBID] = true;
+            }
+            else
+            {
+                updateList.Add(otherRBID, true);
+            }
+            
+        }        
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -20,7 +132,23 @@ public class B18_Treadmill : MonoBehaviour
 
         if (otherRB != null)
         {
-            otherRB.velocity -= (Vector2)gameObject.transform.up * 3f;
+            var otherRBID = otherRB.GetInstanceID();
+
+            Vector2 direction;
+
+            if (moveList.ContainsKey(otherRBID))
+            {
+                var vec = moveList[otherRBID];
+                vec -= (Vector2)gameObject.transform.up;
+                moveList[otherRBID] = vec;
+            }
+            else
+            {
+                var vec = (Vector2)gameObject.transform.up;
+                moveList.Add(otherRBID, Vector2.zero);
+            }
+
         }
     }
+
 }
